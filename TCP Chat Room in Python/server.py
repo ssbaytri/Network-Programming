8e900d83c6b0e276b78877209ceccd6ce0a45294
$ubZ3r0 <106231563+ssbaytri@users.noreply.github.com>
2025-08-02 15:34:09 +0100

    server : done!

--- TCP Chat Room in Python/server.py
+++ TCP Chat Room in Python/server.py
@@ -29,4 +29,22 @@ def handle(client):
 			nicknames.remove(nickname)
 			break
 
+def receive():
+	while True:
+		client, address = server.accept()
+		print(f"Connection with {str(address)}")
 
+		client.send('NICK'.encode('ascii'))
+		nickname = client.recv(1024).decode('ascii')
+		nicknames.append(nickname)
+		clinets.append(client)
+
+		print(f"Nickname of the clinet is {nickname}!")
+		broadcast(f"{nickname} joined the chat!".encode('ascii'))
+		client.send("Connected to the server!".encode('ascii'))
+
+		thread = threading.Thread(target=handle, args=(client,))
+		thread.start()
+
+print("Server is listening...")
+receive()

76b492022ac74128f39f646e911d778208288822
$ubZ3r0 <106231563+ssbaytri@users.noreply.github.com>
2025-08-02 15:54:50 +0100

    client done

---
+++ TCP Chat Room in Python/client.py
@@ -0,0 +1,41 @@
+import socket
+import threading
+
+nickname = input("Choose you nickname: ")
+
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+host = "127.0.0.1"
+port = 5050
+
+client.connect((host, port))
+
+def receive():
+	while True:
+		try:
+			message = client.recv(1024).decode('ascii')
+			if message == "NICK":
+				client.send(nickname.encode('ascii'))
+			else:
+				print(message)
+	
+		except ConnectionAbortedError:
+			print("Connection closed by server.")
+			client.close()
+			break
+		except Exception as e:
+			print(f"Unexpected error: {e}")
+			client.close()
+			break
+
+def write():
+	while True:
+		message = f"{nickname}: {input('Type your message: ')}"
+		client.send(message.encode('ascii'))
+
+
+receive_thread = threading.Thread(target=receive)
+receive_thread.start()
+
+write_thread = threading.Thread(target=write)
+write_thread.start()

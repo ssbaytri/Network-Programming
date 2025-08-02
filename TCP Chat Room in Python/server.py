1329b341700eee54a533c5243da738b3e1110b61
$ubZ3r0 <106231563+ssbaytri@users.noreply.github.com>
2025-08-02 02:58:26 +0100

    TBC

--- TCP Chat Room in Python/server.py
+++ TCP Chat Room in Python/server.py
@@ -7,3 +7,26 @@ port = 5050
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server.bind((host, port))
 server.listen()
+
+clinets = []
+nicknames = []
+
+def broadcast(message):
+	for client in clinets:
+		client.send(message)
+
+def handle(client):
+	while True:
+		try:
+			message = client.recv(1024)
+			broadcast(message)
+		except:
+			index = clinets.index(client)
+			clinets.remove(client)
+			client.close()
+			nickname = nicknames[index]
+			broadcast(f'{nickname} has left the chat!'.encode('ascii'))
+			nicknames.remove(nickname)
+			break
+
+

7fc27b358c49e3455f023a747f987ce7ba8f3d7c
$ubZ3r0 <106231563+ssbaytri@users.noreply.github.com>
2025-08-02 02:50:06 +0100

    simple setup

--- TCP Chat Room in Python/server.py
+++ TCP Chat Room in Python/server.py
@@ -1,2 +1,9 @@
 import threading
 import socket
+
+host = "127.0.0.1" # local host
+port = 5050
+
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.bind((host, port))
+server.listen()

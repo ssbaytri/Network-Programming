b26601d37d1c9ed463a1e27c9e7b0cf670e9d848
$ubZ3r0 <106231563+ssbaytri@users.noreply.github.com>
2025-08-04 05:21:33 +0100

    setup

---
+++ Group Chat App in C/main.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <sys/_endian.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+
+
+int main(void)
+{
+  int sockFD = socket(AF_INET, SOCK_STREAM, 0);
+  
+  char *ip = "127.0.0.1";
+  struct sockaddr_in address;
+  address.sin_family = AF_INET;
+  address.sin_port = htons(5050);
+  inet_pton(AF_INET, ip, &address.sin_addr.s_addr);
+
+  int res = connect(sockFD, (struct sockaddr *)&address, sizeof(address));
+  if (res == 0)
+    printf("Connection was successful!\n");
+  return 0;
+}
